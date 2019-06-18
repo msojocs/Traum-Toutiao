3a9dfc214f419544c4b8e0470badddf3fbef74e3
Traum <1690127128@qqcom>
2019-06-19 07:13:54 +0800

    第一版

---
+++ functions.php
@@ -0,0 +1,194 @@
+<?php
+/*
+    Plugin Name: Traum 头条
+    Plugin URI: https://www.jysafe.cn/3571.air
+    Description: 同步文章到新浪头条
+    Author: Traum
+    Version: 1.0.0
+    Author URI: https://www.jysafe.cn
+    */
+?>
+<?php
+if (!defined('Traum_Toutiao_DIR')) {
+    define('Traum_Toutiao_DIR', plugin_dir_path(__FILE__));
+}
+
+$version = '1.0.0';
+require plugin_dir_path(__FILE__) .'options.php';
+
+add_action('post_submitbox_misc_actions', 'traum_toutiao_publish_metabox_add');
+function traum_toutiao_publish_metabox_add() {
+    //global $post_ID;
+    // 使用随机数进行核查
+    //wp_nonce_field(plugin_basename(__FILE__), 'traum_toutiao_publish_checkbox_noncename');
+    echo '<div class="misc-pub-section"><input type="checkbox" id="traum_toutiao_publish_checkbox" name="traum_toutiao_publish_checkbox" './* checked(1, get_option('traum_toutiao_setting_check_box_enble').*/' />  同时发布头条文章</div>';
+}
+
+/* 写入数据*/
+//add_action( 'save_post', 'traum_toutiao_save_publish_checkbox' );
+/* 文章保存时，保存我们的自定义数据*/
+/*function traum_toutiao_save_publish_checkbox($post_id) {
+
+    // 首先，我们需要检查当前用户是否被授权做这个动作。
+    if ('page' == $_POST['post_type']) {
+        if (! current_user_can('edit_page', $post_id))
+            return;
+    } else {
+        if (! current_user_can('edit_post', $post_id))
+            return;
+    }
+    //ntraum_toutiao_loginfo('开始验证nonce');
+    // 其次，我们需要检查，是否用户想改变这个值。
+    if (! isset($_POST['traum_toutiao_publish_checkbox_noncename']) || ! wp_verify_nonce($_POST['traum_toutiao_publish_checkbox_noncename'], plugin_basename(__FILE__)))
+        return;
+
+    // 第三，我们可以保存值到数据库中
+
+    //如果保存在自定义的表，获取文章ID
+    $post_ID = $_POST['post_ID'];
+    //过滤用户输入
+    $mydata = sanitize_text_field($_POST['traum_toutiao_publish_checkbox']);
+    // 使用$mydata做些什么
+    // 或者使用
+    add_post_meta($post_ID, 'traum_toutiao_publish_checkbox', $mydata, true) or
+    update_post_meta($post_ID, 'traum_toutiao_publish_checkbox', $mydata);
+    // 或自定义表（见下面的进一步阅读的部分）
+}
+*/
+
+/**
+ * WordPress 同步文章到新浪微博头条文章 By 无主题博客
+ * 完善修正 By 祭夜
+ * 修正内容：
+ * 1.部分代码错误
+ * 2.修复同步到头条文章时HTML代码被去掉的问题
+ * 3.增加头条文章封面
+ * 4.添加博客文章的标签关联到新浪话题
+ * 原文地址: http://wuzhuti.cn/2715.html
+ */
+function traum_toutiao_publish($post_ID) {
+    if(!get_option('traum_toutiao_setting_check_box_enble'))
+        if ($_POST['traum_toutiao_publish_checkbox'] != 'on')
+            return;
+    //if (wp_is_post_revision($post_ID))
+    //修订版本(更新)不发微博
+    $get_post_info = get_post($post_ID);
+    $get_post_centent = get_post($post_ID)->post_content;
+    $get_post_title = get_post($post_ID)->post_title;
+    if ($get_post_info->post_status == 'publish') {
+        $appkey = get_option('traum_toutiao_setting_appkey');
+        //key
+        $username = get_option('traum_toutiao_setting_account');
+        //用户名
+        $pass = get_option('traum_toutiao_setting_password');
+        //密码
+
+        $request = new WP_Http;
+
+        //获取文章标签关键词
+        $tags = wp_get_post_tags($post_ID);
+        foreach ($tags as $tag) {
+            $keywords = $keywords.'#'.$tag->name."#";
+        }
+
+        $status = '【' . strip_tags($get_post_title) . '】 ' . mb_strimwidth(strip_tags(apply_filters('the_content', $get_post_centent)) , 0, 132, ' ');
+        $api_url = 'https://api.weibo.com/proxy/article/publish.json';
+
+        $title = strip_tags($get_post_title);
+        //头条的标题
+        $content = get_post($post_ID)->post_content."\r\n原文地址:" . get_permalink($post_ID);
+        //头条的正文
+        $content = handle_content($content);
+        $cover = traum_toutiao_mmimg($post_ID);
+        //头条的封面
+        $summary = mb_strimwidth(strip_tags(apply_filters('the_content', $get_post_centent)) , 0, 110, '...');
+        //头条的导语
+        $text = mb_strimwidth(strip_tags(apply_filters('the_content', $get_post_centent)) , 0, 110, $status).$keywords.'原文地址:' . get_permalink($post_ID);
+        //微博的内容
+
+        $body = array(
+            'title' => $title,
+            'content' => $content,
+            'cover' => $cover,
+            'summary' => $summary,
+            'text' => $text,
+            'source' => $appkey
+        );
+
+        $headers = array('Authorization' => 'Basic ' . base64_encode("$username:$pass"));
+        $result = $request->post($api_url, array('body' => $body,'headers' => $headers));
+        if (get_option('traum_toutiao_setting_log_enble'))traum_toutiao_loginfo($result['body']);
+    }
+}
+add_action('publish_post', 'traum_toutiao_publish', 0);
+//给发布文章增加一个分享微博头条文章的动作
+
+//获取封面
+function traum_toutiao_mmimg($postID) {
+    $cti = traum_catch_that_image($postID);
+    //得到$first_img的值，并赋值给$cti
+    $showimg = $cti;
+    //将$cti的值赋值给$showimg
+    has_post_thumbnail();
+    if (has_post_thumbnail()) {
+        //判断是否有特色图片，有则将$showimg的值替换为特色图片的地址，否则不变
+        $thumbnail_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(),'thumbnail');
+        $shareimg = $thumbnail_image_url[0];
+    } else {
+        $shareimg = $showimg;
+    }
+    ;
+    return $shareimg;
+}
+//调用代码：mmimg($post_ID)
+
+function traum_catch_that_image($postID) {
+    $first_img = '';
+    ob_start();
+    ob_end_clean();
+    $output = preg_match_all('/<img.+src=\"?(.+\.(jpg|gif|bmp|bnp|png))\"?.+>/i',get_post($postID)->post_content,$matches);
+    $first_img = $matches[1][0];
+    //将文章第一张图片的地址赋值给$first_img
+    if (empty($first_img)) {
+        //文章第一张图为空，也就是整篇文章没有图片，将默认设置的图片的地址赋值给$first_img
+        $popimg = get_option('traum_toutiao_setting_cover');
+        $first_img = $popimg;
+    }
+    return $first_img;
+}
+
+//处理content内容
+function handle_content($content) {
+    if (!strpos($content, "<h1>") && strpos($content, "<h2>"))
+        $content = str_replace("<h2>", "<h1>", $content);
+
+    $content = preg_replace("/\[\/?[a-z]+_[a-z]+\]/","",$content);
+    $content = str_replace(array("<br>", "<br />"), "&lt;br&gt;", $content);
+    $content = str_replace(array("\r\n", "\r", "\n"), "<br>", $content);
+    
+    //traum_toutiao_loginfo($content);
+    return $content;
+}
+
+//写日志函数
+function traum_toutiao_loginfo($msg) {
+    $logFile = Traum_Toutiao_DIR.'log/traum_weibo.log';
+    // 日志路径
+    date_default_timezone_set('Asia/Shanghai');
+    file_put_contents($logFile, date('[Y-m-d H:i:s]: ') . $msg . PHP_EOL, FILE_APPEND);
+    //return $msg;
+}
+
+function traum_toutiao_update($ver) {
+    $json = file_get_contents('https://api.jysafe.cn/traum_update/?type=toutiao');
+    $arr = json_decode($json,true);
+
+    _e('The installed version is：','Traum-toutiao');
+    echo $ver.'<br>';
+    _e('The latest version is：','Traum-toutiao');
+    echo $arr['ver'].'<br>';
+    _e('The update description：','Traum-toutiao');
+    echo $arr['description'].'<br>';
+    _e('The download link：','Traum-toutiao');
+    echo "<a href=".$arr['download'].">".__('Click to download','Traum-toutiao').'</a>';
+}
---
+++ options.php
@@ -0,0 +1,227 @@
+<?php
+
+//设置
+function traum_toutiao_menu() {
+    $icon_url = plugins_url('/img/favicon.ico', __FILE__);
+    add_menu_page(
+        'Traum 头条',
+        'Traum 头条',
+        'administrator',
+        'traum_toutiao_setting_page',
+        'traum_toutiao_setting_page_dispaly',
+        $icon_url
+    );
+}
+add_action('admin_menu','traum_toutiao_menu');
+
+function traum_toutiao_plugin_options() {
+    if (false == get_option('traum_toutiao_display_options')) {
+        add_option('traum_toutiao_display_options');
+    }
+
+    //setting
+    add_settings_section(
+        'traum_toutiao_setting_page',                         // ID used to identify this section and with which to register options
+        'Traum toutiao 设置',                               // Title to be displayed on the administration page
+        'traum_toutiao_setting_page_callback',                // Callback used to render the description of the section
+        'traum_toutiao_setting_page'                          // Page on which to add this section of options
+    );
+
+    //enble？
+    add_settings_field(
+        'traum_toutiao_setting_check_box_enble',                      // ID used to identify the field throughout the theme
+        '总是启用插件',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_check_box_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page',
+        array(
+            '启用此处时,写文章页面的同步选项将失效'
+            )
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_check_box_enble'//field ID
+    );
+
+    //账号输入
+    add_settings_field(
+        'traum_toutiao_setting_account',                      // ID used to identify the field throughout the theme
+        '账号',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_account_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page',
+        array(
+            '新浪账号'//传送给数组
+        )
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_account'//field ID
+    );
+
+    //密码表单
+    add_settings_field(
+        'traum_toutiao_setting_password',                      // ID used to identify the field throughout the theme
+        '密码',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_password_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page',
+        array(
+            '新浪密码'//传送给数组
+        )
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_password'//field ID
+    );
+
+    //AppKey表单
+    add_settings_field(
+        'traum_toutiao_setting_appkey',                      // ID used to identify the field throughout the theme
+        'AppKey',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_appkey_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page',
+        array(
+            '<br>新浪AppKey，需要到<a href="https://open.weibo.com/apps" >开发平台</a>获取，且该应用需获得头条文章权限'//传送给数组
+        )
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_appkey'//field ID
+    );
+    
+    //封面表单
+    add_settings_field(
+        'traum_toutiao_setting_cover',                      // ID used to identify the field throughout the theme
+        '默认封面',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_cover_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page',
+        array(
+            '在发布的文章没有任何图片时作为头条文章封面'//传送给数组
+        )
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_cover'//field ID
+    );
+
+    //日志
+    add_settings_field(
+        'traum_toutiao_setting_log_enble',                      // ID used to identify the field throughout the theme
+        '启用日志',                           // The label to the left of the option interface element
+        'traum_toutiao_setting_log_callback',  // The name of the function responsible for rendering the option interface
+        'traum_toutiao_setting_page',                          // The page on which this option will be displayed
+        'traum_toutiao_setting_page'
+    );
+    register_setting(
+        'traum_toutiao_setting_page',//page
+        'traum_toutiao_setting_log_enble'//field ID
+    );
+
+}
+add_action('admin_init', 'traum_toutiao_plugin_options');
+
+function traum_toutiao_setting_page_callback() {
+    echo '设置页面';
+}
+
+
+function traum_toutiao_setting_page_dispaly() {
+    echo '<h2>Let`s start!</h2>';
+    ?>
+    <form method="post" action="options.php">
+        <?php settings_fields('traum_toutiao_setting_page');
+        ?>
+        <?php do_settings_sections('traum_toutiao_setting_page');
+        ?>
+        <?php submit_button();
+        ?>
+    </form>
+    <form method="post">
+    <?php wp_nonce_field(plugin_basename(__FILE__), 'traum_toutiao_setting_noncename'); ?>
+        <input type="submit" class="button button-secondary" value="删除日志" />
+        <input type="hidden" name="traum_toutiao_setting_log_delete" value="log_delete">
+    </form>
+
+    <?php
+    if (! isset($_POST['traum_toutiao_setting_noncename']) || ! wp_verify_nonce($_POST['traum_toutiao_setting_noncename'], plugin_basename(__FILE__)))
+        return;
+    if($_POST['traum_toutiao_setting_log_delete'] == 'log_delete')traum_toutiao_log('delete');
+}
+// end sandbox_general_options_callback
+
+
+function traum_toutiao_setting_check_box_callback($args) {
+    // Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
+    $html = '<input type="checkbox" id="traum_toutiao_setting_check_box_enble" name="traum_toutiao_setting_check_box_enble" value="1" ' . checked(1, get_option('traum_toutiao_setting_check_box_enble'), false) . '/>';
+    echo $html.$args[0];
+}
+// end sandbox_toggle_header_callback
+
+//账号
+function traum_toutiao_setting_account_callback($args) {
+    $html = '<input type="text" id="traum_toutiao_setting_account" name="traum_toutiao_setting_account" value="'.get_option('traum_toutiao_setting_account').'" />';
+    // Here, we'll take the first argument of the array and add it to a label next to the checkbox
+    $html .= '<label for="traum_toutiao_setting_account">' . $args[0] . '</label>';
+    echo $html;
+}
+
+//密码
+function traum_toutiao_setting_password_callback($args) {
+    $html = '<input type="password" id="traum_toutiao_setting_password" name="traum_toutiao_setting_password" value="'.get_option('traum_toutiao_setting_password').'" />';
+    // Here, we'll take the first argument of the array and add it to a label next to the checkbox
+    $html .= '<label for="traum_toutiao_setting_password">' . $args[0] . '</label>';
+    echo $html;
+}
+
+//AppKey
+function traum_toutiao_setting_appkey_callback($args) {
+    $html = '<input type="text" id="traum_toutiao_setting_appkey" name="traum_toutiao_setting_appkey" value="'.get_option('traum_toutiao_setting_appkey').'" />';
+    // Here, we'll take the first argument of the array and add it to a label next to the checkbox
+    $html .= '<label for="traum_toutiao_setting_appkey">' . $args[0] . '</label>';
+    echo $html;
+}
+
+//Cover
+function traum_toutiao_setting_cover_callback($args) {
+    $html = '<input type="text" id="traum_toutiao_setting_cover" name="traum_toutiao_setting_cover" value="'.get_option('traum_toutiao_setting_cover').'" />';
+    // Here, we'll take the first argument of the array and add it to a label next to the checkbox
+    $html .= '<label for="traum_toutiao_setting_cover">' . $args[0] . '</label>';
+    echo $html;
+}
+
+//日志
+function traum_toutiao_setting_log_callback($args) {
+    // Note the ID and the name attribute of the element match that of the ID in the call to add_settings_field
+    $html = '<input type="checkbox" id="traum_toutiao_setting_log_enble" name="traum_toutiao_setting_log_enble" value="1" ' . checked(1, get_option('traum_toutiao_setting_log_enble'), false) . '/>';
+    echo $html.'<br />';
+    traum_toutiao_log('read');
+}
+// end sandbox_toggle_header_callback
+
+//处理日志
+function traum_toutiao_log($action) {
+    $file = Traum_Toutiao_DIR."log/traum_weibo.log";
+    if ($action == 'read') {
+        if (file_exists($file) && get_option('traum_toutiao_setting_log_enble')) {
+            $file = fopen($file, "r") or exit("Unable to open file!");
+            //Output a line of the file until the end is reached
+            //feof() check if file read end EOF
+            while (!feof($file)) {
+                //fgets() Read row by row
+                echo fgets($file). "<br />";
+            }
+            fclose($file);
+        }
+    } else if($action == 'delete') {
+        if (!unlink($file)) {
+            echo ("Error deleting$file");
+        } else
+        {
+            echo ("删除成功$file");
+        }
+    }else return;
+
+}
